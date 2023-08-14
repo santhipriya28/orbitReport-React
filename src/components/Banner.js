1dad4fc713de4e447cc594f8547b44968930f642
santhipriya28 <santhipriya28@gmail.com>
2023-08-13 19:09:41 -0500

    changes done

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,7 @@ export default function Banner()
 {
     return (
         <div><header><h1>Orbit Report</h1></header>
-<p>Click on the buttons to see the satellites in that orbit type</p></div>
+<p>Click on the buttons to see the  satellites in that orbit type</p></div>
 
     );
 }
